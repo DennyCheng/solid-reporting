c960c14bf688252ede3e3abf01ce48a4dd7cea94
Jerry Lee <jerry.lee09@yahoo.com>
2016-09-30 14:49:30 -0500

    upload upload

--- SolidReporting/public/scripts/client_app.js
+++ SolidReporting/public/scripts/client_app.js
@@ -3,19 +3,23 @@ var myApp = angular.module('myApp',['ngMaterial','ngRoute']);
 myApp.config(['$routeProvider', function($routeProvider){
 
   $routeProvider
-  .when('/demographics', {
-        templateUrl: '/public/views/partials/demographics.html',
-        controller: "DemoController"
+      .when('/demographics', {
+            templateUrl: '/public/views/partials/demographics.html',
+            controller: "DemoController"
+          })
+      .when('/login', {
+          templateUrl: '/public/views/login.html',
+          controller: "LoginController"
+        })
+      .when('/register', {
+          templateUrl: '/public/views/register.html',
+          controller: "LoginController"
+        })
+      .when('/upload', {
+          templateUrl: '/public/views/partials/upload.html',
+          controller: "uploadController"
       })
-  .when('/login', {
-      templateUrl: '/public/views/login.html',
-      controller: "LoginController"
-    })
-  .when('/register', {
-      templateUrl: '/public/views/register.html',
-      controller: "LoginController"
-    })
-  .otherwise({
+      .otherwise({
         redirectTo:'login'
       });
 
