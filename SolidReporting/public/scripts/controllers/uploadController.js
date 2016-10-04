77897774c1a9855b68945c4e550b9c69701c323a
Jerry Lee <jerry.lee09@yahoo.com>
2016-10-03 22:18:57 -0500

    add more style and add vailation to uploadpage

--- SolidReporting/public/scripts/controllers/uploadController.js
+++ SolidReporting/public/scripts/controllers/uploadController.js
@@ -1,4 +1,4 @@
-myApp.controller('uploadController', ['$scope', 'fileUpload','DataFactory', '$http', function($scope, fileUpload, DataFactory, $http){
+myApp.controller('uploadController', ['$scope', 'DataFactory', '$http', '$location', function($scope, DataFactory, $http, $location){
 
   $scope.dataFactory = DataFactory;
 
@@ -7,88 +7,103 @@ myApp.controller('uploadController', ['$scope', 'fileUpload','DataFactory', '$ht
 
   $scope.userName = $scope.dataFactory.varUsername();
 
-  $scope.tologout = function() {
-    $scope.dataFactory.logout().then(function(response) {
-      console.log('logged out');
-      console.log('i redirected you to the home page');
-      $location.path("/login");
-    });
+  // $scope.tologout = function() {
+  //   $scope.dataFactory.logout().then(function(response) {
+  //     console.log('logged out');
+  //     console.log('i redirected you to the home page');
+  //     $location.path("/login");
+  //   });
+  //
+  // };
 
-  }
+   // $scope.dataFactory.currentSess();
+   //
+   // $scope.userName = $scope.dataFactory.varUsername();
+   //
+   // $scope.tologout = function() {
+   //   $scope.dataFactory.logout().then(function(response) {
+   //     console.log('logged out');
+   //     console.log('i redirected you to the home page');
+   //     $location.path("/login");
+   //   });
+   //
+   // }
 
-   $scope.dataFactory.currentSess();
+    // $scope.uploadFile = function(){
+    //     var file = $scope.myFile;
+    //
+    //     if(file) {
+    //         var textFile = file.name.substr(file.name.length - 4);
+    //         if (textFile == '.sql') {
+    //             var uploadUrl = "/fileUpload";
+    //             fileUpload.uploadFileToUrl(file, uploadUrl);
+    //         } else {
+    //             alert('Please insert sql file');
+    //         }
+    //     } else {
+    //         alert('Please enter file');
+    //     }
+    // };
 
-   $scope.userName = $scope.dataFactory.varUsername();
-
-   $scope.tologout = function() {
-     $scope.dataFactory.logout().then(function(response) {
-       console.log('logged out');
-       console.log('i redirected you to the home page');
-       $location.path("/login");
-     });
-
-   }
-
-    $scope.uploadFile = function(){
-        var file = $scope.myFile;
-
-        if(file) {
-            var textFile = file.name.substr(file.name.length - 4);
+    $scope.onSubmit = function(){
+        var file = $scope.file[0];
+        console.log(file);
+        if (file) {
+            var textFile = file.lfFileName.substr(file.lfFileName.length - 4);
             if (textFile == '.sql') {
-                var uploadUrl = "/fileUpload";
-                fileUpload.uploadFileToUrl(file, uploadUrl);
+                var formData = new FormData();
+                angular.forEach($scope.file, function (obj) {
+                    formData.append('file', obj.lfFile);
+                });
+
+                $http.post('./fileUpload', formData, {
+                    transformRequest: angular.identity,
+                    headers: {'Content-Type': undefined}
+                }).then(function (result) {
+                    // do someting
+                    $location.path("/demographics");
+                }, function (err) {
+                    // do someting
+                });
             } else {
                 alert('Please insert sql file');
             }
         } else {
-            alert('Please enter file');
+            alert('Please insert file');
         }
-    };
 
-    $scope.onSubmit = function(){
-        var formData = new FormData();
-        angular.forEach($scope.file,function(obj){
-            formData.append('file', obj.lfFile);
-        });
-        $http.post('./fileUpload', formData, {
-            transformRequest: angular.identity,
-            headers: {'Content-Type': undefined}
-        }).then(function(result){
-            // do sometingh
-        },function(err){
-            // do sometingh
-        });
+
     };
 
 }]);
 
-myApp.directive('fileModel', ['$parse', function ($parse) {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-            var model = $parse(attrs.fileModel);
-            var modelSetter = model.assign;
+// myApp.directive('fileModel', ['$parse', function ($parse) {
+//     return {
+//         restrict: 'A',
+//         link: function(scope, element, attrs) {
+//             var model = $parse(attrs.fileModel);
+//             var modelSetter = model.assign;
+//
+//             element.bind('change', function(){
+//                 scope.$apply(function(){
+//                     modelSetter(scope, element[0].files[0]);
+//                 });
+//             });
+//         }
+//     };
+// }]);
 
-            element.bind('change', function(){
-                scope.$apply(function(){
-                    modelSetter(scope, element[0].files[0]);
-                });
-            });
-        }
-    };
-}]);
-
-myApp.service('fileUpload', ['$http', function ($http) {
-    this.uploadFileToUrl = function(file, uploadUrl){
-        var fd = new FormData();
-        fd.append('file', file);
-        $http.post(uploadUrl, fd, {
-            transformRequest: angular.identity,
-            headers: {'Content-Type': undefined}
-        })
-            .success(function(){
-            })
-            .error(function(){
-            });
-    }
-}]);
+// myApp.service('fileUpload', ['$http', function ($http) {
+//     this.uploadFileToUrl = function(file, uploadUrl){
+//         var fd = new FormData();
+//         fd.append('file', file);
+//         $http.post(uploadUrl, fd, {
+//             transformRequest: angular.identity,
+//             headers: {'Content-Type': undefined}
+//         })
+//             .success(function(){
+//             })
+//             .error(function(){
+//             });
+//     }
+// }]);
