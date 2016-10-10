d2892d5894767adebbcec34e5c408da77aa302f0
Jerry Lee <jerry.lee09@yahoo.com>
2016-10-10 10:27:33 -0500

    edit landing page

--- SolidReporting/public/scripts/controllers/democontroller.js
+++ SolidReporting/public/scripts/controllers/democontroller.js
@@ -2635,8 +2635,6 @@ myApp.controller("DemoController", ["$scope",'$http','DataFactory', '$location',
     return diff;
   };
   
-
-
     $scope.exportData = function () {
       var blob = new Blob([document.getElementById('exportable').innerHTML], {
         type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8"
