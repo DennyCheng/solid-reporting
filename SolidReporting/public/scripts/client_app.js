b520753d7edf0c1b8dea50bf14f4d139e0d00cb7
Jerry Lee <jerry.lee09@yahoo.com>
2016-10-12 09:08:27 -0500

    add opacity

--- SolidReporting/public/scripts/client_app.js
+++ SolidReporting/public/scripts/client_app.js
@@ -58,3 +58,14 @@ myApp.config(function($mdThemingProvider) {
     .primaryPalette('indigo')
     .accentPalette('light-green')
 });
+
+myApp.directive('toggleClass', function() {
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+      element.bind('click', function() {
+        element.toggleClass(attrs.toggleClass);
+      });
+    }
+  };
+});
