b0d60fd375b5bc87f064f3d813e8582e21655a44
Liz Haakenson <lhaakens3@gmail.com>
2016-10-17 18:18:40 -0500

    changes again

--- SolidReporting/modules/connection.js
+++ SolidReporting/modules/connection.js
@@ -4,14 +4,7 @@ var connectionString = '';
 if(process.env.DATABASE_URL != undefined) {
     connectionString = process.env.DATABASE_URL + "?ssl=true";
 } else {
-    connectionString = {
-      user: '', //env var: PGUSER
-      database: 'd5hb67qv48kp30', //env var: PGDATABASE
-      password: '', //env var: PGPASSWORD
-      port: 5432, //env var: PGPORT
-      max: 1000, // max number of clients in the pool
-      idleTimeoutMillis: 1000, // how long a client is allowed to remain idle before being closed
-    };
+    connectionString = 'postgres://localhost:5432/solidGround4';
 }
 
 module.exports = connectionString;
