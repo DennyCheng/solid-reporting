c4542e692fd133c5a0a9484c0b51fbdaa58e7e33
Jerry Lee <jerry.lee09@yahoo.com>
2016-10-17 18:58:45 -0500

    add local files into app no more https files

---
+++ SolidReporting/public/vendors/alasql/src/46apply.js
@@ -0,0 +1,22 @@
+/*
+//
+// CROSS AND OUTER APPLY for Alasql.js
+// Date: 03.11.2014
+// (c) 2014, Andrey Gershun
+//
+*/
+
+yy.Apply = function (params) { 
+	return yy.extend(this, params); 
+}
+
+yy.Apply.prototype.toString = function () {
+	var s = this.applymode+' APPLY ('+this.select.toString()+')';
+	
+	if(this.as) 
+		s += ' AS '+this.as;
+	
+	return s;
+};
+
+
