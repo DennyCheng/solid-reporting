16d2925893c5f5c721eb2fa91e7253ed79aba421
Liz Haakenson <lhaakens3@gmail.com>
2016-10-18 14:01:06 -0500

    change to connectionString

--- SolidReporting/modules/connection.js
+++ SolidReporting/modules/connection.js
@@ -4,7 +4,7 @@ var connectionString = '';
 if(process.env.DATABASE_URL != undefined) {
     connectionString = process.env.DATABASE_URL + "?ssl=true";
 } else {
-    connectionString = 'postgres://127.0.0.1:5432/solidGround4';
+    connectionString = 'postgres://localhost:5432/solidGround4';
 }
 
 module.exports = connectionString;
