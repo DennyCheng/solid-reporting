05aa775cf2e5732c4c8561ac7fe5140420d3012a
Liz Haakenson <lhaakens3@gmail.com>
2016-10-18 14:25:47 -0500

    test

--- SolidReporting/modules/connection.js
+++ SolidReporting/modules/connection.js
@@ -1,6 +1,6 @@
 // connection.js
 var connectionString = '';
-
+console.log('process.env: ', process.env.DATABASE_URL);
 if(process.env.DATABASE_URL != undefined) {
     connectionString = process.env.DATABASE_URL + "?ssl=true";
 } else {
